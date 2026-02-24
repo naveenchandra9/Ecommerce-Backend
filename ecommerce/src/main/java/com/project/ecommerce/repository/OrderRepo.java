d64f95097fdcbd77d1d7e6a1d51605ac97f01299
Naveenchandra Halemani <nhalemani@gmail.com>
2026-02-24 12:21:37 +0530

    Implemented OrderService, added findByUserId method to OrderRepo, Added two new custom exceptions and minor fix for entities

--- ecommerce/src/main/java/com/project/ecommerce/repository/OrderRepo.java
+++ ecommerce/src/main/java/com/project/ecommerce/repository/OrderRepo.java
@@ -4,6 +4,11 @@ import com.project.ecommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer> {
+
+    List<Order> findByUserId(int user_id);
 }
