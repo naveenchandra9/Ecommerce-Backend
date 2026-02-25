25522c6f0673a19d41ee3e0369326f8fbc030d8d
Naveenchandra Halemani <nhalemani@gmail.com>
2026-02-25 09:11:12 +0530

    Implemented CategoryService and UserService and necessary changes in Category and User Entities

--- ecommerce/src/main/java/com/project/ecommerce/repository/CategoryRepo.java
+++ ecommerce/src/main/java/com/project/ecommerce/repository/CategoryRepo.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Integer> {
     Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
 }
