e368a6aa83bed1eb34774c37b5889c1cf5b437c2
Naveenchandra Halemani <nhalemani@gmail.com>
2026-06-26 08:25:43 +0530

    Added LoggingInterceptor to log duration between request and response, Configured the interceptor in WebMvcConfig

---
+++ ecommerce/src/main/java/com/project/ecommerce/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.project.ecommerce.config;
+
+import com.project.ecommerce.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**");
+    }
+}
--- ecommerce/src/main/java/com/project/ecommerce/service/EmailService.java
+++ ecommerce/src/main/java/com/project/ecommerce/service/EmailService.java
@@ -1,10 +1,14 @@
 package com.project.ecommerce.service;
 
+import com.project.ecommerce.entity.OrderItems;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
