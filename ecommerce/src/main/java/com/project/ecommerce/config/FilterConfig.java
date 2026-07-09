f26cb53bcfc8f658f25d129e80ca98f8aded117d
Naveenchandra Halemani <nhalemani@gmail.com>
2026-07-09 22:52:37 +0530

    Implemented custom CORS filter to filter all http requests and responses

--- ecommerce/src/main/java/com/project/ecommerce/config/FilterConfig.java
+++ ecommerce/src/main/java/com/project/ecommerce/config/FilterConfig.java
@@ -1,4 +1,21 @@
 package com.project.ecommerce.config;
 
+import com.project.ecommerce.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+
+    }
 }
